17e8d599ddfcb372845013ef560a31a41438e470
Filipe Deboni <deboni.filipe@gmail.com>
2020-11-04 18:31:50 -0300

    small changes in Skills and Project details

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -51,8 +51,8 @@ function Project2() {
                 Clover is a personal finance control application developed in
                 one week from concept to the final website based on the 50/30/20
                 rule. It was developed in a team of two as a requirement of the
-                6th week of the Ironhack Web Development Bootcamp and consist on
-                an application where the user can insert their revenue and
+                6th week of the Ironhack Web Development Bootcamp and consists
+                on an application where the users can insert their revenue and
                 expenses and the website calculate their budget on each custom
                 category:
                 <ul className="my-1">
@@ -60,15 +60,15 @@ function Project2() {
                     Essentials (50%): basic expenses as rent and groceries
                   </li>
                   <li>
-                    Lifestyle (30%): that things that make your life better as
-                    dinner out, streaming services and vacation
+                    Lifestyle (30%): things that make your life better as dining
+                    out, streaming services and vacation
                   </li>
                   <li>Priorities (20%): savings and investments</li>
                 </ul>
-                The user can change all the categories at will. as requirements,
-                we needed to use Handlebars. I personally preferred to use React
-                like in other Projects developed during the course for a better
-                organization. As the next features, we planned improvements on
+                The user can change all the categories at will. As requirements
+                of the Project, we needed to use Handlebars. I personally
+                preferred to use React like in other Projects developed during
+                the course. As the next features, we planned improvements on
                 reports and graphics.
               </p>
             </div>
